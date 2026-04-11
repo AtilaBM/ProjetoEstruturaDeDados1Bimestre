db52aaf6914f1db54a02ce06ab3ae885abb0f98e
Manoela Pereira de Sousa <manoela2016pereira@gmail.com>
2026-04-11 01:06:52 -0300

    Update lista.h

--- lista.h
+++ lista.h
@@ -5,8 +5,29 @@ typedef struct {
 
     char sigla_tribunal[10];
     char municipio_oj[100];
+    
+    // Variáveis da Meta 1
     int casos_novos_2026;
     int julgados_2026;
+    int dessobrestados_2026;
+    int suspensos_2026;
+
+    // Variáveis da Meta 2A e 2Ant
+    int julgm2_a;
+    int distm2_a;
+    int suspm2_a;
+    int julgm2_ant;
+    int distm2_ant;
+    int suspm2_ant;
+    int desom2_ant;
+
+    // Variáveis da Meta 4A e 4B
+    int julgm4_a;
+    int distm4_a;
+    int suspm4_a;
+    int julgm4_b;
+    int distm4_b;
+    int suspm4_b;
 
 }Tribunal;
 
@@ -23,5 +44,11 @@ typedef struct {
 void initLista(Lista *L);
 void inserirNoFim(Lista *L, Tribunal T);
 void lerArquivoCSV(char *nomearq, Lista *L);
+
+float calcularMeta1(Lista *L);
+float calcularMeta2A(Lista *L);
+float calcularMeta2Ant(Lista *L);
+float calcularMeta4A(Lista *L);
+float calcularMeta4B(Lista *L);
 
 #endif

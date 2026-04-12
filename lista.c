46515521a859043e9badee563756399cdd752951
Manoela Pereira de Sousa <manoela2016pereira@gmail.com>
2026-04-12 12:42:02 -0300

    Update lista.c

--- lista.c
+++ lista.c
@@ -8,14 +8,14 @@ void initLista(Lista *L){
 
 void inserirNoFim(Lista *L, Tribunal T){
     No* novo = (No *)malloc(sizeof(No));
+    if(novo == NULL) return; 
     novo->dados = T;    
     novo->prox = NULL;
     
-   
     if(L->inicio == NULL){
         L->inicio = novo;
-    } else {
-       
+    } 
+       else {
         No* atual = L->inicio; 
         while(atual->prox != NULL){
             atual = atual->prox;
@@ -32,59 +32,54 @@ void lerArquivoCSV(char *nomearq, Lista *L) {
     }
 
     char cabecalho[500];
-    fgets(cabecalho, 500, f);
-
+    fgets(cabecalho, 500, f); 
     Tribunal T;
-    while (fscanf(f, " \"%[^\"]\",", T.sigla_tribunal) != EOF) {
-    
    
-    fscanf(f, " \"%[^\"]\",", T.procedimento);
-    fscanf(f, " \"%[^\"]\",", T.ramo_justica);
-    fscanf(f, " \"%[^\"]\",", T.sigla_grau);
-    fscanf(f, " \"%[^\"]\",", T.uf_oj);
-    fscanf(f, " \"%[^\"]\",", T.municipio_oj);
+    while (fscanf(f, " \"%[^\"]\",", T.sigla_tribunal) == 1) {
+        
+        fscanf(f, " \"%[^\"]\",", T.procedimento);
+        fscanf(f, " \"%[^\"]\",", T.ramo_justica);
+        fscanf(f, " \"%[^\"]\",", T.sigla_grau);
+        fscanf(f, " \"%[^\"]\",", T.uf_oj);
+        fscanf(f, " \"%[^\"]\",", T.municipio_oj);
+        fscanf(f, " %d,", &T.id_ultimo_oj);
+        fscanf(f, " \"%[^\"]\",", T.nome);
+        fscanf(f, " \"%[^\"]\",", T.mesano_cnm1);
+        fscanf(f, " \"%[^\"]\",", T.mesano_sent);
+        fscanf(f, " %d,", &T.casos_novos_2026);
+        fscanf(f, " %d,", &T.julgados_2026);
+        fscanf(f, " %d,", &T.prim_sent2026);
+        fscanf(f, " %d,", &T.suspensos_2026);
+        fscanf(f, " %d,", &T.dessobrestados_2026);
+        fscanf(f, " %f,", &T.cumprimento_meta1); 
 
+        // BLOCO META 2A
+        fscanf(f, " %d,", &T.distm2_a);
+        fscanf(f, " %d,", &T.julgm2_a);
+        fscanf(f, " %d,", &T.suspm2_a);
+        fscanf(f, " %f,", &T.cumprimento_meta2a);
+
+        // BLOCO META 2 ANT
+        fscanf(f, " %d,", &T.distm2_ant);
+        fscanf(f, " %d,", &T.julgm2_ant);
+        fscanf(f, " %d,", &T.suspm2_ant);
+        fscanf(f, " %d,", &T.desom2_ant);
+        fscanf(f, " %f,", &T.cumprimento_meta2ant);
+
+        // BLOCO META 4A
+        fscanf(f, " %d,", &T.distm4_a);
+        fscanf(f, " %d,", &T.julgm4_a);
+        fscanf(f, " %d,", &T.suspm4_a);
+        fscanf(f, " %f,", &T.cumprimento_meta4a);
+        
+        // BLOCO META 4B
+        fscanf(f, " %d,", &T.distm4_b);
+        fscanf(f, " %d,", &T.julgm4_b);
+        fscanf(f, " %d,", &T.suspm4_b);
+        fscanf(f, " %f\n", &T.cumprimento_meta4b); 
+
+        inserirNoFim(L, T);
+    }
     
-    fscanf(f, " %d,", &T.id_ultimo_oj);
-
-    // TEXTOS NOVAMENTE
-    fscanf(f, " \"%[^\"]\",", T.nome);
-    fscanf(f, " \"%[^\"]\",", T.mesano_cnm1);
-    fscanf(f, " \"%[^\"]\",", T.mesano_sent);
-
-    fscanf(f, " %d,", &T.casos_novos_2026);
-    fscanf(f, " %d,", &T.julgados_2026);
-    fscanf(f, " %d,", &T.prim_sent2026);
-    fscanf(f, " %d,", &T.suspensos_2026);
-    fscanf(f, " %d,", &T.dessobrestados_2026);
-    
-    
-    fscanf(f, " %f,", &T.cumprimento_meta1); 
-
-    // BLOCO META 2A
-    fscanf(f, " %d,", &T.distm2_a);
-    fscanf(f, " %d,", &T.julgm2_a);
-    fscanf(f, " %d,", &T.suspm2_a);
-    fscanf(f, " %f,", &T.cumprimento_meta2a);
-
-    // BLOCO META 2 ANT
-    fscanf(f, " %d,", &T.distm2_ant);
-    fscanf(f, " %d,", &T.julgm2_ant);
-    fscanf(f, " %d,", &T.suspm2_ant);
-    fscanf(f, " %d,", &T.desom2_ant);
-    fscanf(f, " %f,", &T.cumprimento_meta2ant);
-
-   
-    fscanf(f, " %d,", &T.distm4_a);
-    fscanf(f, " %d,", &T.julgm4_a);
-    fscanf(f, " %d,", &T.suspm4_a);
-    fscanf(f, " %f,", &T.cumprimento_meta4a);
-    
-    fscanf(f, " %d,", &T.distm4_b);
-    fscanf(f, " %d,", &T.julgm4_b);
-    fscanf(f, " %d,", &T.suspm4_b);
-    fscanf(f, " %f", &T.cumprimento_meta4b); 
-
-   
-    inserirNoFim(L, T);
+    fclose(f); 
 }
